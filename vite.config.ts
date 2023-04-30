814076f899dcd99369f293a4a6b8bd5455e060fc
Ahmedtaher820 <et8989784@gmail.com>
2023-04-30 04:52:39 +0300

    add v-it-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import { fileURLToPath, URL } from "node:url";
   import vue from "@vitejs/plugin-vue";
   // new stuff
   import { resolve, dirname } from 'node:path';
-  import vueI18n from '@intlify/vite-plugin-vue-i18n';
-// https://vitejs.dev/config/
+  import vueI18n from '@intlify/vite-plugin-vue-i18n'
+  // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
     vueI18n({
