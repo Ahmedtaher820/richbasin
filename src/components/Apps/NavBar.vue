93045d5f929edd5913595be9cac63208cd98f4c1
Ahmedtaher820 <et8989784@gmail.com>
2023-04-30 06:15:11 +0300

    update navbar toggle and style

--- src/components/Apps/NavBar.vue
+++ src/components/Apps/NavBar.vue
@@ -139,7 +139,7 @@ const mobileMenuOpen = ref(false)
                 <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
                   <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
                     <div class="absolute right-0 top-0 flex w-16 justify-center pt-5">
-                      <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
+                      <button type="button" class="-m-2.5 p-2.5" @click="mobileMenuOpen = false">
                         <span class="sr-only">Close sidebar</span>
                         <XMarkIcon class="h-6 w-6 text-primary" aria-hidden="true" />
                       </button>
