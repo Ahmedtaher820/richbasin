e0ff72ecfb767d9494fbdaf827dae4b88137ffd5
Ahmedtaher820 <et8989784@gmail.com>
2023-05-04 03:50:11 +0300

    update nav-list

--- src/components/Apps/NavBar.vue
+++ src/components/Apps/NavBar.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 import {
   Dialog,
   DialogPanel,
@@ -10,7 +10,7 @@ import {
   PopoverButton,
   PopoverGroup,
   PopoverPanel,
-} from '@headlessui/vue'
+} from "@headlessui/vue";
 import {
   Menu,
   MenuButton,
@@ -18,7 +18,7 @@ import {
   MenuItems,
   TransitionChild,
   TransitionRoot,
-} from '@headlessui/vue'
+} from "@headlessui/vue";
 import {
   ArrowPathIcon,
   Bars3Icon,
@@ -29,146 +29,225 @@ import {
   XMarkIcon,
   UserGroupIcon,
   GlobeAltIcon,
-  SpeakerWaveIcon
-} from '@heroicons/vue/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/vue/20/solid'
+  SpeakerWaveIcon,
+  ChevronUpIcon,
+} from "@heroicons/vue/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/vue/20/solid";
 // import i18n from '../../i18n';
 
 const about = [
-  { name: 'Leadership', href: '/leadership', icon: UserGroupIcon },
-  { name: 'Health Safety And Environment', href: '/health-safety-and-environment-hse', icon: GlobeAltIcon },
-  { name: 'Vision', href: '/vision', icon: ChartPieIcon },
-  { name: 'Mission', href: '/mission', icon: SpeakerWaveIcon },
-  { name: 'Value', href: '/value', icon: CursorArrowRaysIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-]
+  { name: "Leadership", href: "/leadership", icon: UserGroupIcon },
+  {
+    name: "Health Safety And Environment",
+    href: "/health-safety-and-environment-hse",
+    icon: GlobeAltIcon,
+  },
+  { name: "Vision", href: "/vision", icon: ChartPieIcon },
+  { name: "Mission", href: "/mission", icon: SpeakerWaveIcon },
+  { name: "Value", href: "/value", icon: CursorArrowRaysIcon },
+];
+const callsToAction = [{ name: "Watch demo", href: "#", icon: PlayCircleIcon }];
 const nav = [
   {
-        path:"/",
-        name:'Home',
-    },
-    {
-        path:"/services",
-        name:'Services',
-    },
-    {
-        path:"/projects",
-        name:'Projects',
-    },
-    {
-        path:"/csr",
-        name:'CSR',
-    },
-    {
-        path:"/news",
-        name:'News',
-    },
-    {
-        path:"/career",
-        name:'Career',
-    },
-    {
-        path:"/contact",
-        name:'Contact',
-    },  
-]
-const mobileMenuOpen = ref(false)
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/services",
+    name: "Services",
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+  },
+  {
+    path: "/csr",
+    name: "CSR",
+  },
+  {
+    path: "/news",
+    name: "News",
+  },
+  {
+    path: "/career",
+    name: "Career",
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+  },
+];
+const mobileMenuOpen = ref(false);
+const list = ref();
+const showIcon = ref(true)
+const showNavList = () => {
+  (list.value as HTMLElement).classList.toggle("active");
+  showIcon.value = !showIcon.value
+};
 </script>
 
 <template>
-    <header class="bg-white sticky w-full top-0 left-0 shadow-sm z-50">
-        <nav class="mx-auto flex w-full items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div class="flex lg:flex-1">
-            <router-link to="/" class="-m-1.5 p-1.5">
-              <span class="sr-only">Your Company</span>
-              <img class="w-20" src="../../../public/logo.jfif" alt="" />
-            </router-link>
+  <header class="bg-white sticky w-full top-0 left-0 shadow-sm z-50">
+    <nav
+      class="mx-auto flex w-full items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div class="flex lg:flex-1">
+        <router-link to="/" class="-m-1.5 p-1.5">
+          <span class="sr-only">Your Company</span>
+          <img class="w-20" src="../../../public/logo.jfif" alt="" />
+        </router-link>
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div class="hidden lg:flex items-center flex-1 gap-8 justify-end">
+        <div class="relative ">
+          <div @click="showNavList" class="flex gap-1">
+            <span>
+              About
+            </span>
+            <ChevronDownIcon class="w-4" v-if="showIcon"  />
+            <ChevronUpIcon class="w-4" v-else />
           </div>
-          <div class="flex lg:hidden">
-            <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-              <span class="sr-only">Open main menu</span>
-              <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-            </button>
+          <div
+            class="nav-list absolute z-50 w-96 list text-primary -bottom-80 rounded-lg shadow-inner bg-white pt-6 "
+            ref="list"
+          >
+            <ul class="flex flex-col overflow-hidden">
+              <li v-for="item in about" :key="item.name" class="flex items-center gap-2 hover:bg-slate-500 hover:text-white transition-all duration-300 py-3 pl-6 cursor-pointer">
+                <component :is="item.icon" class="w-6 h-6" />
+                <router-link class="tracking-wide w-full" :to="{ path: item.href }">
+                <span class="block" @click="showNavList">
+                    {{
+                    item.name
+                  }}
+                </span>
+              </router-link>
+              </li>
+            </ul>
           </div>
-          <PopoverGroup class="hidden lg:flex lg:gap-x-12">
-            <Popover class="relative flex">
-              <PopoverButton class="flex items-center gap-x-1 text-1xl focus:outline-none  border-none text-gray-900">
-                   About
-                  <ChevronDownIcon  class="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+        </div>
 
-              </PopoverButton>
-    
-              <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-                <PopoverPanel class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                  <div class="p-4">
-                    <div v-for="item in about" :key="item.name" class="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50">
-                      <div class="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <component :is="item.icon" class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                      </div>
-                      <div class="flex-auto">
-                        <router-link :to="{path:item.href}" class="block text-1xl font-semibold text-gray-900">
-                          {{item.name}}
-                          <span class="absolute inset-0" />
-                        </router-link>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                    <a v-for="item in callsToAction" :key="item.name" :href="item.href" class="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 ">
-                      <component :is="item.icon" class="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-                      {{ item.name }}
-                    </a>
-                  </div>
-                </PopoverPanel>
-              </transition>
-            </Popover>
-    
-          <router-link v-for="item in nav"  :key="item.name" :to="item.path" class="-mx-3 block rounded-lg p-2 text-base leading-7 text-gray-900 link relative">{{`${item.name}`}}</router-link>
-          </PopoverGroup>
-        </nav>
-        <TransitionRoot as="template" :show="mobileMenuOpen">
-          <Dialog as="div" class="relative z-50 lg:hidden" @close="mobileMenuOpen = false">
-            <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
-              <div class="fixed inset-0 bg-gray-900/80" />
-            </TransitionChild>
-    
-            <div class="fixed inset-0 flex">
-              <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-                <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
-                  <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
-                    <div class="absolute right-0 top-0 flex w-16 justify-center pt-5">
-                      <button type="button" class="-m-2.5 p-2.5" @click="mobileMenuOpen = false">
-                        <span class="sr-only">Close sidebar</span>
-                        <XMarkIcon class="h-6 w-6 text-primary" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </TransitionChild>
-                  <!-- Sidebar component, swap this element with another sidebar if you like -->
-                  <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-               
-                    <Disclosure as="div"  v-slot="{ open }">
-                      <DisclosureButton class="flex w-full mt-16 items-center justify-between rounded-lg py-2 text-base  hover:bg-gray-50">
-                        About
-                        <ChevronDownIcon :class="[open ? 'rotate-180' : '', 'h-5 w-5 flex-none']" aria-hidden="true" />
-                      </DisclosureButton>
-                      <DisclosurePanel class="mt-2 space-y-2">
-                        <DisclosureButton v-for="item in [...about, ...callsToAction]" :key="item.name" as="a" :href="item.href" class="block rounded-lg py-2 pl-8 pr-3 text-sm font-semibold leading-7 text-gray-600 hover:bg-primary-50">{{ item.name }}</DisclosureButton>
-                      </DisclosurePanel>
-                    </Disclosure>
-                    <router-link v-for="item in nav"  :key="item.name" :to="item.path" class=" block rounded-lg py-2 text-1xl font-semibold  leading-7 text-gray-900">
-                      <span @click="mobileMenuOpen = false">
-                        {{`${item.name}`}}
-                      </span> 
-                    </router-link>
-                  </div>
-                </DialogPanel>
+        <router-link
+          v-for="item in nav"
+          :key="item.name"
+          :to="item.path"
+          class="-mx-3 block rounded-lg p-2 text-base leading-7 text-gray-900 link relative"
+          >{{ `${item.name}` }}</router-link
+        >
+      </div>
+    </nav>
+    <TransitionRoot as="template" :show="mobileMenuOpen">
+      <Dialog
+        as="div"
+        class="relative z-50 lg:hidden"
+        @close="mobileMenuOpen = false"
+      >
+        <TransitionChild
+          as="template"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 bg-gray-900/80" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 flex">
+          <TransitionChild
+            as="template"
+            enter="transition ease-in-out duration-300 transform"
+            enter-from="-translate-x-full"
+            enter-to="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leave-from="translate-x-0"
+            leave-to="-translate-x-full"
+          >
+            <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
+              <TransitionChild
+                as="template"
+                enter="ease-in-out duration-300"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="ease-in-out duration-300"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+              >
+                <div
+                  class="absolute right-0 top-0 flex w-16 justify-center pt-5"
+                >
+                  <button
+                    type="button"
+                    class="-m-2.5 p-2.5"
+                    @click="mobileMenuOpen = false"
+                  >
+                    <span class="sr-only">Close sidebar</span>
+                    <XMarkIcon
+                      class="h-6 w-6 text-primary"
+                      aria-hidden="true"
+                    />
+                  </button>
+                </div>
               </TransitionChild>
-            </div>
-          </Dialog>
-        </TransitionRoot>
-        <!-- <Dialog as="div" class="lg:hidden " @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+              <!-- Sidebar component, swap this element with another sidebar if you like -->
+              <div
+                class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4"
+              >
+                <Disclosure as="div" v-slot="{ open }">
+                  <DisclosureButton
+                    class="flex w-full mt-16 items-center justify-between rounded-lg py-2 text-base hover:bg-gray-50"
+                  >
+                    About
+                    <ChevronDownIcon
+                      :class="[open ? 'rotate-180' : '', 'h-5 w-5 flex-none']"
+                      aria-hidden="true"
+                    />
+                  </DisclosureButton>
+                  <DisclosurePanel class="mt-2 space-y-2">
+                    <DisclosureButton
+                      v-for="item in [...about, ...callsToAction]"
+                      :key="item.name"
+                      as="a"
+                      :href="item.href"
+                      class="block rounded-lg py-2 pl-8 pr-3 text-sm font-semibold leading-7 text-gray-600 hover:bg-primary-50"
+                      ><span @click="!open">
+                        {{ item.name }}
+                      </span>
+                    </DisclosureButton>
+                  </DisclosurePanel>
+                </Disclosure>
+
+                <router-link
+                  v-for="item in nav"
+                  :key="item.name"
+                  :to="item.path"
+                  class="block rounded-lg py-2 text-1xl font-semibold leading-7 text-gray-900"
+                >
+                  <span @click="mobileMenuOpen = false">
+                    {{ `${item.name}` }}
+                  </span>
+                </router-link>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </Dialog>
+    </TransitionRoot>
+    <!-- <Dialog as="div" class="lg:hidden " @close="mobileMenuOpen = false" :open="mobileMenuOpen">
           <div class="fixed inset-0 " />
           <DialogPanel class="fixed inset-y-0 mob-nav left-0 w-full overflow-y-auto bg-white px-8 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div class="flex items-center justify-end">
@@ -188,22 +267,46 @@ const mobileMenuOpen = ref(false)
             </div>
           </DialogPanel>
         </Dialog> -->
-      </header>
+  </header>
 </template>
 <style>
-.router-link-active{
-  @apply relative font-semibold
+.router-link-active {
+  @apply relative font-semibold;
 }
 
-.link::after{
-  content: '';
+.link::after {
+  content: "";
   @apply absolute bottom-1 left-1/2 w-0  h-0.5 bg-primary transition-all duration-300;
   transform: translateX(-50%);
 }
-.link:hover::after{
-  @apply w-1/2
+.link:hover::after {
+  @apply w-1/2;
 }
-.mob-nav{
+.mob-nav {
   z-index: 999;
+}
+.nav-list {
+  transform: translateY(10%);
+  opacity: 0;
+  display: none;
+
+  pointer-events: none;
+  transform-style: all 0.5s ease-in-out;
+}
+.nav-list.active {
+  display: block;
+  pointer-events: auto;
+
+  animation: fadeLeft .5s ease-in-out both;
+}
+@keyframes fadeLeft {
+  from{
+    transform: translateY(10%);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0%);
+    opacity: 1;
+  }
 }
 </style>
